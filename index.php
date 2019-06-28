89a7acf1e03b2c281843bac18cf162952f5d5825
addehd <hej@adolfo.se>
2019-06-28 16:58:14 +0200

    show wp posts

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Hello, World!
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : ?>
+	<?php
+	while ( have_posts() ) : the_post();?>
+		<article class="post">
+		<h2><?php the_title(); ?></h2>
+		<section class="content"><?php the_content(); ?></section>
+		</article>
+	<?php
+	endwhile; 
+endif; 
+?>
+
+<?php get_footer(); ?>	
