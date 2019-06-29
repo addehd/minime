6e37e55c558fbdfe0568a6d8bdac338a765a7716
addehd <hej@adolfo.se>
2019-06-29 10:59:14 +0200

    html structure

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo get_the_title(); ?></title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
+	<?php wp_head(); ?>
+</head>
+<body>
