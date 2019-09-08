fa53e63086f0496bc28e1c40400c542f0b080cde
addehd <hej@adolfo.se>
2019-09-08 23:59:32 +0200

    cleaner

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
-	<?php
-	while ( have_posts() ) : the_post();?>
-		<article class="post">
-		<h2><?php the_title(); ?></h2>
-		<section class="content"><?php the_content(); ?></section>
-		</article>
-	<?php
-	endwhile; 
-endif; 
-?>
+<?php if( have_posts()) {
+	while( have_posts()) {
+		the_post();
+		the_title();
+		the_content();
+	}
+}?>
 
 <?php get_footer(); ?>	
