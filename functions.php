8b96ab51548520241ddad65b266234e0bd4dcfeb
addehd <hej@adolfo.se>
2019-10-01 12:52:09 +0200

    svg support

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php 
 
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
 add_theme_support( 'post-thumbnails' );
 
 function register_my_menu() {
