07991d03c963914c45ed445e00644c37897db7f2
addehd <hej@adolfo.se>
2020-04-01 14:42:09 +0200

    simplified theme

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo Get_bloginfo(); ?></title>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
 	<?php wp_head(); ?>
 </head>
 
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <?php if( have_posts()) {
 	while( have_posts()) {
+		the_post();
 		the_content();
 	}
 }?>
