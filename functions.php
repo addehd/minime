fda74459d7df3407134294942d87a87d2eb2ec14
addehd <master_unppgeafmg@363795.cloudwaysapps.com>
2020-05-14 11:38:44 +0000

    simplifying checkout page in woocommerce

--- functions.php
+++ functions.php
@@ -31,6 +31,48 @@ add_filter( 'acf/rest_api/field_settings/show_in_rest', '__return_true' );
 // Enable the option edit in rest
 add_filter( 'acf/rest_api/field_settings/edit_in_rest', '__return_true' );
 
+ /**
+ Remove all possible fields
+ **/
+function wc_remove_checkout_fields( $fields ) {
+
+    // Billing fields
+    unset( $fields['billing']['billing_company'] );
+	unset( $fields['billing']['billing_country'] );
+    //unset( $fields['billing']['billing_email'] );
+    //unset( $fields['billing']['billing_phone'] );
+    unset( $fields['billing']['billing_state'] );
+    //unset( $fields['billing']['billing_first_name'] );
+    unset( $fields['billing']['billing_last_name'] );
+    //unset( $fields['billing']['billing_address_1'] );
+    unset( $fields['billing']['billing_address_2'] );
+    unset( $fields['billing']['billing_city'] );
+    //unset( $fields['billing']['billing_postcode'] );
+
+    // Shipping fields
+    unset( $fields['shipping']['shipping_company'] );
+    unset( $fields['shipping']['shipping_phone'] );
+    unset( $fields['shipping']['shipping_state'] );
+    unset( $fields['shipping']['shipping_first_name'] );
+    unset( $fields['shipping']['shipping_last_name'] );
+    unset( $fields['shipping']['shipping_address_1'] );
+    unset( $fields['shipping']['shipping_address_2'] );
+    unset( $fields['shipping']['shipping_city'] );
+    unset( $fields['shipping']['shipping_postcode'] );
+
+    // Order fields
+    unset( $fields['order']['order_comments'] );
+
+    return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'wc_remove_checkout_fields' );
+
+add_action( 'woocommerce_single_product_summary', 'add_to_cart_button_woocommerce', 20 );
+function add_to_cart_button_woocommerce() {
+    echo '
+WooCommerce customize add to cart button
+';
+}
 
 function cc_mime_types($mimes) {
   $mimes['svg'] = 'image/svg+xml';
